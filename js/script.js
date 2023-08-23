54b34307a04728fcb01d108b92db283e02e2833c
basilnajeem <basilnajeem@gmail.com>
2023-08-23 16:51:03 +0530

    To do

--- js/script.js
+++ js/script.js
@@ -1,21 +1,45 @@
-const taskInput = document.getElementById("taskInput");
-const addButton = document.getElementById("addButton");
+$(document).ready(function () {
+    const taskInput = $("#taskinput");
+    const taskList = $('#tasklist');
+    const noResultsMessage = $('<li class="no-results">No results found</li>'); // Create the message element
 
+    $("#addButton").on("click", function () {
+        const taskText = taskInput.val();
+        if (taskText === "") {
+            alert("Please enter a task");
+            return;
+        }
 
+        const li = `<li>${taskText}</li>`;
+        taskList.append(li);
+        taskList.show();
+        taskInput.val("");
+    });
 
-addButton.addEventListener("click", addTask);
+    const searchButton = $('#searchButton');
 
-function addTask() {
-    const taskText = taskInput.value;
-    if (taskText === "") {
-        alert("please enter a task");
-        return;
-    }
-    
-    const taskList = document.getElementById("taskList");
-    const li = document.createElement("li");
-    li.innerHTML = `<span>${taskText}</span>`;
+    searchButton.on("click", function () {
+        const searchTerm = taskInput.val().toLowerCase();
+        let hasResults = false; // Flag to track if any results were found
 
-    taskList.appendChild(li);
-    taskInput.value = "";
-}
+        // Hide all tasks
+        taskList.children().hide();
+
+        // Show tasks that match the search term
+        taskList.children().each(function () {
+            const task = $(this);
+            const taskText = task.text().toLowerCase();
+            if (taskText.includes(searchTerm)) {
+                task.show();
+                hasResults = true;
+            }
+        });
+
+        // Display "No Results" message if no tasks match
+        if (!hasResults) {
+            taskList.append(noResultsMessage);
+        } else {
+            noResultsMessage.remove(); // Remove the message if results are found
+        }
+    });
+});
